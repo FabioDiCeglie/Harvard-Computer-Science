c358a242326edf61bb0bd01ef6da42281fb46a6f
FabioDiCeglie <fabiod@backbase.com>
2024-06-19 19:29:13 +0200

    Complete volume exercise week4

---
+++ week4/volume/volume.c
@@ -0,0 +1,55 @@
+/**
+ * Run in the terminal "make volume" to create the binary code, add a .wav file and next Run "./volume [inputs]" to execute the program.
+ * The results will be a moduled audio file with volume changed
+ * How to use it? 'make volume' './volume input.wav output.wav 2.0'
+*/
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+   // Check command-line arguments
+   if (argc != 4)
+   {
+      printf("Usage: ./volume input.wav output.wav factor\n");
+      return 1;
+   }
+
+   // Open files and determine scaling factor
+   FILE *input = fopen(argv[1], "r");
+   if (input == NULL)
+   {
+      printf("Could not open file.\n");
+      return 1;
+   }
+
+   FILE *output = fopen(argv[2], "w");
+   if (output == NULL)
+   {
+      printf("Could not open file.\n");
+      return 1;
+   }
+
+   float factor = atof(argv[3]);
+
+   uint8_t header[HEADER_SIZE];
+   fread(&header, sizeof(header), 1, input);
+   fwrite(&header, sizeof(header), 1, output);
+
+
+   int16_t buffer;
+   while (fread(&buffer, sizeof(buffer), 1, input))
+   {
+      buffer *= factor;
+      fwrite(&buffer, sizeof(buffer), 1, output);
+   }
+
+   // Close files
+   fclose(input);
+   fclose(output);
+}
